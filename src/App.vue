2f6ea4ec04ed206528f87bbe01e1e1d30b12eef6
Thomas <thomas.saury@epita.fr>
2022-09-12 15:11:10 +0200

    Test branch

--- src/App.vue
+++ src/App.vue
@@ -106,8 +106,8 @@ body {
   overflow: hidden;
   /* scroll-behavior: smooth; */
   position: absolute;
-  height: 100%;
-  width: 100%;
+  height: 100vh;
+  width: 100vw;
   padding: 0;
   margin: 0;
 }
